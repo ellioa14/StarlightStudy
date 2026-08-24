f343797ebc0a81f63423f71b587565259d540196
ellioa14 <ellioa14@gmail.com>
2026-08-23 22:30:05 -0400

    Fix Compose activity import

--- aurora-build/app/src/main/java/com/example/aurorascout/MainActivity.kt
+++ aurora-build/app/src/main/java/com/example/aurorascout/MainActivity.kt
@@ -7,16 +7,15 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
